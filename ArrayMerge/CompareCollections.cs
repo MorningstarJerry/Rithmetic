fea26fe788b18009d8b75d7976d502a94bc06dbe
Jerry <JerryDanks@Outlook.com>
2017-05-14 18:03:38 +0800

    RI-001 Init rep

---
+++ ArrayMerge/CompareCollections.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayMerge
+{
+    public class CompareCollections
+    {
+        //两个无序数组的合并算法
+        public static List<int> Merge(int[] a, int[] b)
+        {
+            List<int> c = new List<int>();
+            int i = 0;
+            int j = 0;
+
+            Array.Sort(a);
+            Array.Sort(b);
+
+            if (a == null || a.Length == 0)
+            {
+                return b.ToList<int>();
+            }
+            if (b == null || b.Length == 0)
+            {
+                return a.ToList<int>();
+            }
+
+            if (a[0] > b[0])
+            {
+                c.Add(b[0]);
+                j++;
+            }
+            else
+            {
+                c.Add(a[0]);
+                i++;
+            }
+
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    c.Add(a[i]);
+                    i++;
+                }
+                else
+                {
+                    c.Add(b[j]);
+                    j++;
+                }
+            }
+
+            while (i < a.Length)
+            {
+                c.Add(a[i]);
+                i++;
+            }
+
+            while (j < b.Length)
+            {
+                c.Add(b[j]);
+                j++;
+            }
+
+            return c;
+        }
+
+        /// <summary>
+        /// 去重复实现
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <returns></returns>
+        public static List<string> distinct(string origin)
+        {
+            List<string> nowList = new List<string>();
+            char[] array = origin.ToCharArray();
+            nowList.Add(array[0].ToString());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = 0;
+                while (j < nowList.Count)
+                {
+                    if (array[i].ToString() == nowList[j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        j++;
+                    }
+
+                    if (j == nowList.Count)
+                    {
+                        nowList.Add(array[i].ToString());
+                    }
+                }
+            }
+            return nowList;
+        }
+
+
+        /// <summary>
+        /// 分割算法
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="sp"></param>
+        /// <returns></returns>
+        public static string[] SplitBy(string input, string sp)
+        {
+            if (string.IsNullOrWhiteSpace(input) || sp == null)
+            {
+                return null;
+            }
+            List<string> output = new List<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int j = input.IndexOf(sp, i);
+                string temp = "";
+                if (j >= 0)
+                {
+                    temp = input.Substring(i, j - i);
+                    output.Add(temp);
+                    i = j + sp.Length - 1;
+                }
+                else
+                {
+                    temp = input.Substring(i);
+                    if (!string.IsNullOrEmpty(temp))
+                    {
+                        output.Add(temp);
+                    }
+                    break;
+                }
+            }
+            return output.ToArray<string>();
+        }
+
+        /// <summary>
+        /// int parse 实现
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static long TransToNumber(string str)
+        {
+            if (str == null || str.Length == 0)
+            {
+                return -1;
+            }
+
+            int result = 0;
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (0 <= (str[j] - '0') && (str[j] - '0') <= 9)
+                {
+                    checked
+                    {
+                        result = result * 10 + (str[j] - '0');
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            return result;
+        }
+    }
+}
--- ArrayMerge/Program.cs
+++ ArrayMerge/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArrayMerge
 {
@@ -10,6 +7,31 @@ namespace ArrayMerge
     {
         static void Main(string[] args)
         {
+            int[] a = { 24, 14, 25, 17, 58 };
+            int[] b = { 21, 12, 23, 77, 38 };
+            Console.WriteLine("=========================Two Array Merge Test");
+            Console.WriteLine("Array a = [" + string.Join(",", a) + "]");
+            Console.WriteLine("Array b = [" + string.Join(",", b) + "]");
+            Console.WriteLine("Merge the two Array [" + string.Join(",", CompareCollections.Merge(a, b).ToArray<int>()) + "]");
+            Console.ReadKey();
+
+
+            Console.WriteLine("====================string distinct Test, [ABBBCCCCDEFDAB]");
+            Console.WriteLine("string distinct test after distinct, [" + string.Join(",",CompareCollections.distinct("ABBBCCCCDEFDAB").ToArray<string>()) + "]");
+            Console.ReadKey();
+
+
+
+            Console.WriteLine("==================split Test [ABBBCCCCDEFDAB]");
+            Console.WriteLine("string after split BY D  , [" + string.Join(",", CompareCollections.SplitBy("ABBBCCCCDEFDAB","D").ToArray<string>()) + "]");
+            Console.WriteLine("string after split BY DE  , [" + string.Join(",", CompareCollections.SplitBy("ABBBCCCCDEFDAB", "DE").ToArray<string>()) + "]");
+            Console.ReadKey();
+
+
+            Console.WriteLine("===============Trans To a Number Test");
+            Console.WriteLine("string before trans 1234");
+            Console.WriteLine("After Trans , [" +  CompareCollections.TransToNumber("1234")+ "]");
+            Console.ReadKey();
         }
     }
 }
